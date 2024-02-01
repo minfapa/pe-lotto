cb4bbfb174ccff4e4319a5dfce5031cc18f161b5
aaron <st.aaron.jung@gmail.com>
2024-02-02 00:08:23 +0900

    FE: 설문지 와꾸 완성

--- packages/react-app/src/pages/SurveyPage.tsx
+++ packages/react-app/src/pages/SurveyPage.tsx
@@ -1,7 +1,48 @@
+import { Survey } from "../components/Survey";
+import { CircularProgress, Stack, Typography } from "@mui/material";
+import logo from "../assets/ci_logo.png";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 export function SurveyPage() {
-  return (
-    <div>
-      <h1>Survey Page</h1>
-    </div>
-  );
+    const [lottoNumber, setLottoNumber] = useState<number | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const location = useLocation();
+    useEffect(() => {
+        setLoading(true);
+        const queryParams = new URLSearchParams(location.search);
+        const number = queryParams.get("number");
+        if (number) {
+            setLottoNumber(parseInt(number));
+        } else {
+            setLottoNumber(null);
+        }
+        setLoading(false);
+    }, [location.search]);
+
+
+    return (
+        <>
+            {loading && <CircularProgress />}
+            {!loading && !lottoNumber && (
+                <Stack direction={"column"} spacing={1} sx={{ padding: 3 }}>
+                    <img src={logo} alt={"logo"}
+                         style={{ maxWidth: "100%", height: "auto" }} />
+                    <Typography fontSize={"x-large"} fontWeight={"bolder"} align={"center"}>
+                        {"잘못된 번호 입니다. 관리자에게 문의하세요."}
+                    </Typography>
+                </Stack>)
+            }
+            {!loading && lottoNumber && (
+                <Stack direction={"column"} spacing={1} sx={{ padding: 3 }}>
+                    <img src={logo} alt={"logo"}
+                         style={{ maxWidth: "100%", height: "auto" }} />
+                    <Typography fontSize={"x-large"} fontWeight={"bolder"} align={"center"}>
+                        {`체육대회 행운권: ${lottoNumber}번`}
+                    </Typography>
+                    <Survey />
+                </Stack>
+            )}
+        </>
+    );
 }
